64bbafbb32a102478c74802c17a40c9cb6ec0724
BarsTiger <kit.barsic@gmail.com>
2023-02-14 13:56:43 +0200

    Changed bot db sync answer text

--- bot/handlers/initialize/all_messages.py
+++ bot/handlers/initialize/all_messages.py
@@ -4,4 +4,5 @@ from bot.db.pull_db import pull
 
 async def sync_db_filter(message: Message):
     await pull()
-    await message.reply(f'🔄️ Bot database synchronised. If you tried to run a command, run it again')
+    await message.reply(f'🔄️ Bot database synchronised because of restart. '
+                        f'If you tried to run a command, run it again')
